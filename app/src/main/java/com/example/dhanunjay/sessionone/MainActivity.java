3c2f556922a9540a261212e2128a688698962070
msg4dhanu <msg4dhanu@gmail.com>
2017-02-28 23:09:48 -0500

    second one from dhanunjay

--- app/src/main/java/com/example/dhanunjay/sessionone/MainActivity.java
+++ app/src/main/java/com/example/dhanunjay/sessionone/MainActivity.java
@@ -7,6 +7,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //second one from dhanunjay
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
